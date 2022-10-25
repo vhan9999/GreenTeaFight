022fb66b34df47321e068dd3a74dd2694a54830f
nonorze-VOXX <appieosugood@gmail.com>
2022-10-26 01:10:21 +0800

    attack animation only can work

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -5,26 +5,7 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject Player;
     private bool attacked = false;
-    public float size = 1;
-    private int counter = 0;
-    private float[] attackTimes ={
-        0.0f,
-        0.1f,
-        0.2f,
-        0.3f,
-        0.4f,
-        0.5f
-    };
-    private Vector3[] attack_location = {
-        new Vector3(1,1,0),
-        new Vector3(2,0.5f,0),
-        new Vector3(3,0,0),
-        new Vector3(2,-0.5f,0),
-        new Vector3(1,-1,0) };
-    private float keepTime = 5;
-    private float nowTime = 0;
     void Start()
     {
         newAttack();
@@ -33,82 +14,10 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        nowTime += Time.deltaTime;
-
-        if (nowTime >= attackTimes[counter])
-        {
-            Debug.Log(nowTime);
-            Attack(counter);
-            counter += 1;
-        }
-    }
-    void Attack(int counter)
-    {
-        if (counter == 0)
-        {
-            transform.position = new Vector3(
-                Player.transform.position.x + attack_location[counter].x * size,
-                Player.transform.position.y + attack_location[counter].y * size,
-                Player.transform.position.z
-            );
-            transform.Rotate(0, 0, 0, Space.Self);
-        }
-        else if (counter == 1)
-        {
-            transform.position = new Vector3(
-                Player.transform.position.x + attack_location[counter].x * size,
-                Player.transform.position.y + attack_location[counter].y * size,
-                Player.transform.position.z
-                );
-            transform.Rotate(0, 0, -45, Space.Self);
-        }
-        else if (counter == 2)
-        {
-            transform.position = new Vector3(
-                Player.transform.position.x + attack_location[counter].x * size,
-                Player.transform.position.y + attack_location[counter].y * size,
-                Player.transform.position.z
-                );
-            transform.Rotate(0, 0, 45, Space.Self);
-        }
-        else if (counter == 3)
-        {
-            transform.position = new Vector3(
-                Player.transform.position.x + attack_location[counter].x * size,
-                Player.transform.position.y + attack_location[counter].y * size,
-                Player.transform.position.z
-                );
-            transform.Rotate(0, 0, 45, Space.Self);
-        }
-        else if (counter == 4)
-        {
-            transform.position = new Vector3(
-                Player.transform.position.x + attack_location[counter].x * size,
-                Player.transform.position.y + attack_location[counter].y * size,
-                Player.transform.position.z
-                );
-            transform.Rotate(0, 0, -45, Space.Self);
-        }
-        else
-        {
-            this.gameObject.SetActive(false);
-
-        }
-
     }
     public void newAttack()
     {
         attacked = false;
-        counter = 0;
-        nowTime = 0f;
-        this.gameObject.SetActive(true);
-
-        transform.position = new Vector3(
-            Player.transform.position.x + attack_location[counter].x * size,
-            Player.transform.position.y + attack_location[counter].y * size,
-            Player.transform.position.z
-        );
-        transform.Rotate(0, 0, 0, Space.Self);
     }
 
     public void OnCollisionStay2D(Collision2D collision)
--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -4,56 +4,116 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public PlayerAttack attack;
+    enum PlayerState
+    {
+        Idle = 0,
+        Walk = 1,
+        Run = 2,
+        Attack = 3
+    }
+    PlayerState nowPlayerState = PlayerState.Idle;
+    //public PlayerAttack attack;
     private Rigidbody2D Rigidbody;
     private bool TouchGround;
     public PlayerData data;
+    //public GameObject attack;
+
+    public Animator playerAction;
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        playerAction = gameObject.GetComponent<Animator>();
         NewGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        //Debug.Log(nowPlayerState);
+        playerAction.SetInteger("State", (int)nowPlayerState);
+        switch (nowPlayerState)
+        {
+            case PlayerState.Idle:
+                break;
+            case PlayerState.Walk:
+                break;
+            case PlayerState.Run:
+                break;
+            case PlayerState.Attack:
+                Debug.Log(playerAction.GetCurrentAnimatorStateInfo(0).IsName("AttackEnd"));
+                if (playerAction.GetCurrentAnimatorStateInfo(0).IsName("AttackEnd"))
+                {
+                    nowPlayerState = PlayerState.Idle;
+                }
+
+
+                Attack();
+                /*
+                if ( == playerAction.GetCurrentAnimatorStateInfo(0))
+                {
+                    nowPlayerState = PlayerState.Idle;
+
+                }
+                */
+                break;
+        }
+        AllInput();
     }
-    private void Move()
+
+    private void AllInput()
     {
+
         if (Input.GetKey("d") && Input.GetKey("a"))
         {
-            Rigidbody.velocity = new Vector2(0.0f, Rigidbody.velocity.y);
+            Move(0.0f, Rigidbody.velocity.y);
+            //Rigidbody.velocity = new Vector2(0.0f, Rigidbody.velocity.y);
         }
         else if (Input.GetKey("d"))
         {
-            Rigidbody.velocity = new Vector2(data.MoveSpeed, Rigidbody.velocity.y);
+            Move(data.MoveSpeed, Rigidbody.velocity.y);
+            //Rigidbody.velocity = new Vector2(data.MoveSpeed, Rigidbody.velocity.y);
         }
         else if (Input.GetKey("a"))
         {
-            Rigidbody.velocity = new Vector2(-data.MoveSpeed, Rigidbody.velocity.y);
+            Move(-data.MoveSpeed, Rigidbody.velocity.y);
+            //Rigidbody.velocity = new Vector2(-data.MoveSpeed, Rigidbody.velocity.y);
         }
         else
         {
-            Rigidbody.velocity = new Vector2(0.0f, Rigidbody.velocity.y);
+            Move(0.0f, Rigidbody.velocity.y);
+            //Rigidbody.velocity = new Vector2(0.0f, Rigidbody.velocity.y);
         }
         if (Input.GetKey("w") && TouchGround)
         {
-            Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, data.JumpSpeed);
+            Move(Rigidbody.velocity.x, data.JumpSpeed);
+            //Rigidbody.velocity = new Vector2(Rigidbody.velocity.x, data.JumpSpeed);
             TouchGround = false;
         }
 
         if (Input.GetKey("j"))
         {
-            attack.newAttack();
+            nowPlayerState = PlayerState.Attack;
+            //attack.SetBool("Attack", true);
         }
+
+    }
+    private void Move(float x, float y)
+    {
+
+        Rigidbody.velocity = new Vector2(x, y);
+        //
+
+    }
+    public void Attack()
+    {
     }
     public void NewGame()
     {
         gameObject.transform.Translate(0.0f, 0.0f, 0.0f);
         TouchGround = false;
+        nowPlayerState = PlayerState.Idle;
     }
     public void OnCollisionStay2D(Collision2D collision)
     {
