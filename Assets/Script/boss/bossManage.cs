a314e4eb222a7797dc47884df58af003c1aa66ef
vhan9999 <66200083+vhan9999@users.noreply.github.com>
2022-10-31 04:26:56 +0800

    boss slash animation done

--- Assets/Script/boss/bossManage.cs
+++ Assets/Script/boss/bossManage.cs
@@ -5,29 +5,89 @@ using UnityEngine;
 public class bossManage : MonoBehaviour
 {
     public float speed;
+    public float distance;
     public GameObject player;
+
+    private Animator bossAnimator;
     private Transform playerT;
-    private float distance = 3;
+    private Transform bossT;
+    private bool TouchGround;
+    private bool leftDir;
+    enum bossState
+    {
+        Idle = 0,
+        Walk = 1,
+        Slash = 2
+    }
+    bossState nowBossState;
     // Start is called before the first frame update
     void Start()
     {
-
+        NewGame();
+        bossAnimator = gameObject.GetComponent<Animator>();
+        playerT = player.GetComponent<Transform>();
+        bossT = this.gameObject.transform;
     }
     // Update is called once per frame
     void Update()
     {
-        playerT = player.GetComponent<Transform>();
-        float bossX = this.gameObject.transform.position.x;
-        if (Mathf.Abs(playerT.position.x - bossX) > distance)
+        Debug.Log(bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("bossActiveEnd"));
+        Debug.Log(nowBossState);
+        bossAnimator.SetInteger("state", (int)nowBossState);
+        switch (nowBossState)
         {
-            if (bossX > playerT.position.x)
-            {
-                this.gameObject.transform.Translate(new Vector2(-speed, 0));
-            }
-            if (bossX < playerT.position.x)
-            {
-                this.gameObject.transform.Translate(new Vector2(speed, 0));
-            }
+            case bossState.Idle:
+                if (TouchGround)
+                    nowBossState = bossState.Walk;
+                break;
+            case bossState.Walk:
+                if (bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("bossWalk"))
+                    Move();
+                break;
+            case bossState.Slash:
+                if(bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("bossActiveEnd"))
+                    nowBossState = bossState.Walk;
+                break;
         }
     }
+    private void NewGame()
+    {
+        nowBossState = bossState.Idle;
+        leftDir = true;
+        TouchGround = false;
+    }
+    private void Move()
+    {
+        if (bossT.position.x > playerT.position.x)
+        {
+            if (leftDir == false)
+            {
+                leftDir = true;
+                bossT.Rotate(0, 180, 0);
+            }
+            if (bossT.position.x - playerT.position.x > distance)
+                bossT.Translate(new Vector2(-speed, 0));
+            else
+                nowBossState = bossState.Slash;
+        }
+        else
+        {
+            if (leftDir == true)
+            {
+                leftDir = false;
+                bossT.Rotate(0, 180, 0);
+            }
+            if (playerT.position.x - bossT.position.x > distance)
+                bossT.Translate(new Vector2(-speed, 0));
+            else
+                nowBossState = bossState.Slash;
+        }
+    }
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            TouchGround = true;
+        }
+    } 
 }
